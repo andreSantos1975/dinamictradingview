c86b2a6bcf858955ce512b48ee8408ed63cdc031
Andre Luis <meiahora7203@gmail.com>
2023-03-25 21:09:08 -0300

    server2.0

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-
+import { useEffect } from 'react';
+import { getCandle } from './server/DataServer';
 import './App.css';
 import ApexChart from './Chart';
 
 function App() {
+
+  useEffect(() => {
+     getCandle('BTCUSDT', '1m')
+     .then(data => console.log(data))
+     .catch(err => alert(err.response ? err.response.data : err.message))
+  },[])
   return (
     <div className="App">
      <ApexChart />
