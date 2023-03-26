9a13f2d21ff42da25d1102bb171e8a9a4244f617
Andre Luis <meiahora7203@gmail.com>
2023-03-26 13:05:44 -0300

    isLoading

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getCandle } from './server/DataServer';
 import './App.css';
 import ApexChart from './Chart';
-
 function App() {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-     getCandle('BTCUSDT', '1m')
-     .then(data => console.log(data))
-     .catch(err => alert(err.response ? err.response.data : err.message))
-  },[])
+     getCandle('ETHUSDT', '1m')
+     .then(data => {
+        setData(data);
+        setIsLoading(false);
+     })
+     .catch(err => {
+        setIsLoading(false);
+        alert(err.response ? err.response.data : err.message);
+     })
+  }, []);
+
   return (
     <div className="App">
-     <ApexChart />
+      {!isLoading && <ApexChart data={data} />}
+      {isLoading && <p>Loading...</p>}
     </div>
   );
 }
+
 
 export default App;
